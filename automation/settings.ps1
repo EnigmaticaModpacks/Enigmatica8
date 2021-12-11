11087e5fc2fecdd5a515d44d667dcd3c1c6108de
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2021-12-11 22:51:01 +0100

    prep for forge update hotfix

--- automation/settings.ps1
+++ automation/settings.ps1
@@ -29,11 +29,11 @@ $MODPACK_NAME = "Enigmatica8"
 $CLIENT_NAME = "Enigmatica8"
 
 # Version Of The Modpack
-$MODPACK_VERSION = "1.0.2"
+$MODPACK_VERSION = "1.0.3"
 
 # Last Version Of The Modpack
 # Needed For Changelog Parsing
-$LAST_MODPACK_VERSION = "1.0.1"
+$LAST_MODPACK_VERSION = "1.0.2"
 
 # =====================================================================//
 #  CHANGELOG SETTINGS

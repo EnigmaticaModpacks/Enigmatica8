02e9ad21c3abd83253bf213b0ba85643812748e6
Kanzaji ^^ <60540476+Kanzaji@users.noreply.github.com>
2022-08-16 19:47:48 +0200

    Change custom tags to `enigmatica:`

--- kubejs/server_scripts/base/tags/items/tconstruct/tinkers_anvil.js
+++ kubejs/server_scripts/base/tags/items/tconstruct/tinkers_anvil.js
@@ -1,3 +1,3 @@
 onEvent('item.tags', (event) => {
-    event.add('tconstruct:tinkers_anvils', ['tconstruct:scorched_anvil', 'tconstruct:tinkers_anvil']);
+    event.add('enigmatica:tinkers_anvils', ['tconstruct:scorched_anvil', 'tconstruct:tinkers_anvil']);
 });

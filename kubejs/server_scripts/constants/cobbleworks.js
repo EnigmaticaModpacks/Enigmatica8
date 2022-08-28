6ab27a6b2143da0f54e210823361dbeb34ab0814
Kanzaji ^^ <60540476+Kanzaji@users.noreply.github.com>
2022-08-28 21:49:10 +0200

    Removed even more duplicates

--- kubejs/server_scripts/constants/cobbleworks.js
+++ kubejs/server_scripts/constants/cobbleworks.js
@@ -2,36 +2,36 @@
 
 // Items added here will get recipes for Pedestals & Cobblegen Randomizer cobblegens.
 const generatableCobblestone = [
-    'byg:dacite_cobblestone',
-    'byg:ether_stone',
-    'byg:purpur_stone',
-    'byg:red_rock',
-    'byg:scoria_cobblestone',
-    'byg:soapstone',
-    'byg:travertine',
-    'undergarden:depthrock',
-    'undergarden:shiverstone',
-    'undergarden:tremblecrust'
+    // 'byg:dacite_cobblestone',
+    // 'byg:ether_stone',
+    // 'byg:purpur_stone',
+    // 'byg:red_rock',
+    // 'byg:scoria_cobblestone',
+    // 'byg:soapstone',
+    // 'byg:travertine',
+    // 'undergarden:depthrock',
+    // 'undergarden:shiverstone',
+    // 'undergarden:tremblecrust'
 ];
 // Items added here will get recipes for Pedestals & Cobblegen Randomizer stonegens.
 const generatableStone = [
     'byg:black_sandstone',
     'byg:blue_sandstone',
-    'byg:dacite',
+    // 'byg:dacite',
     'byg:purple_sandstone',
     'byg:pink_sandstone',
-    'byg:scoria_stone',
+    // 'byg:scoria_stone',
     'byg:white_sandstone',
     'minecraft:red_sandstone',
     'minecraft:sandstone',
     'minecraft:basalt',
     'byg:brimstone',
-    'quark:jasper',
-    'quark:limestone',
-    'quark:permafrost',
-    'create:scoria',
-    'create:limestone',
-    'quark:myalite',
-    'quark:dusky_myalite'
+    // 'quark:jasper',
+    // 'quark:limestone',
+    // 'quark:permafrost',
+    // 'create:scoria',
+    // 'create:limestone',
+    // 'quark:myalite',
+    // 'quark:dusky_myalite'
 ];
 const generatableBasalt = [];

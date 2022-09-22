a8ac2c439204dfea0be80143ebdf741454a34298
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2022-09-22 22:08:26 +0200

    move to newest format

--- kubejs/server_scripts/base/recipes/minecraft/shapeless.js
+++ kubejs/server_scripts/base/recipes/minecraft/shapeless.js
@@ -1,3 +1,9 @@
-onEvent("recipes", event => {
-    event.shapeless('minecraft:crafting_table', ['#forge:workbench']);
+onEvent('recipes', (event) => {
+    const id_prefex = 'enigmatica:base/minecraft/shapeless/';
+    const recipes = [
+        { output: 'minecraft:crafting_table', inputs: ['#forge:workbench'], id: `${id_prefex}crafting_table` }
+    ];
+    recipes.forEach((recipe) => {
+        event.shapeless(recipe.output, recipe.inputs).id(recipe.id);
+    });
 });

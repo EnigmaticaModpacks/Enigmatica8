6056cffa418bc85abe9f0bdc9b238b8bf2e4d236
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2022-10-20 21:24:17 +0200

    fixed #309

--- kubejs/server_scripts/base/tags/blocks/forge/storage_blocks.js
+++ kubejs/server_scripts/base/tags/blocks/forge/storage_blocks.js
@@ -2,4 +2,5 @@ onEvent('block.tags', (event) => {
     event.add('forge:storage_blocks', ['minecraft:glowstone']);
 
     event.add('forge:storage_blocks/glowstone', ['minecraft:glowstone']);
+    event.add('forge:storage_blocks/uranium', ['ftbic:uranium_block']);
 });
